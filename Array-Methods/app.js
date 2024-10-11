f970153acd5d1324b1710d4b1ce4c95b45f7834e
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-10-11 19:33:30 +0500

    new-assignment

---
+++ Array-Methods/app.js
@@ -0,0 +1,111 @@
+
+//Array Method Presentation
+
+// concat() Returns a new array comprised of this array joined with other array(s) and/or value(s)
+// every() Returns true if every element in this array satisfies the provided testing function
+// filter() Creates a new array with all of the elements of this array for which the provided filtering function returns true
+// forEach() Calls a function for each element in the array
+// indexOf() Returns the first (least) index of an element within the array equal to the specified value, or -1 if none is found
+// join() Joins all elements of an array into a string
+// lastIndexOf() Returns the last (greatest) index of an element within the array equal to the specified value, or -1 if none is found
+// map() Creates a new array with the results of calling a provided function on every element in this array
+// pop() Removes the last element from an array and returns that element
+// push() Adds one or more elements to the end of an array and returns the new length of the array
+// reduce() Applies a function simultaneously against two values of the array (from left-to-right) as to reduce it to a single value
+// reduceRight() Applies a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value
+// reverse() Reverses the order of the elements of an array -- the first becomes the last, and the last becomes the first
+// shift() Removes the first element from an array and returns that element
+// slice() Extracts a section of an array and returns a new array
+// some() Returns true if at least one element in this array satisfies the provided testing function
+// toSource() Represents the source code of an object
+// sort() Sorts the elements of an array
+// splice() Adds and/or removes elements from an array
+// toString() Returns a string representing the array and its elements
+// unshift() Adds one or more elements to the front of an array and returns the new length of the array
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var length = fruits.push("Kiwi");
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.join(" * ");
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var fruit = fruits[2];
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var size = fruits.length;
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var fruit = fruits.pop();
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.toString();
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var fruit = fruits.shift();
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");
+console.log(fruits);
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[0] = "Kiwi";
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[fruits.length] = "Kiwi";
+
+// Warning !
+// Using delete() leaves undefined holes in the array.
+
+// Use pop() or shift() instead.
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+delete fruits[0];
+
+// The concat() method does not change the existing arrays. It always returns a new array.
+
+// The concat() method can take any number of array arguments.
+
+var myGirls = ["Cecilie", "Lone"];
+var myBoys = ["Emil", "Tobias", "Linus"];
+
+var myChildren = myGirls.concat(myBoys);
+
+// The copyWithin() method overwrites the existing values.
+// The copyWithin() method does not add items to the array.
+// The copyWithin() method does not change the length of the array.
+
+// Copy to index 2, all elements from index 0:
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.copyWithin(2, 0);
+
+// Copy to index 2, the elements from index 0 to 2:
+var fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+fruits.copyWithin(2, 0, 2);
+
+var myArr = [[1,2],[3,4],[5,6]];
+var newArr = myArr.flat();
+
+var myArr = [1, 2, 3, 4, 5, 6];
+var newArr = myArr.flatMap(x => [x, x * 10]);
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 2, "Lemon", "Kiwi");
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 2, "Lemon", "Kiwi");
+var months = ["Jan", "Feb", "Mar", "Apr"];
+var spliced = months.toSpliced(0, 1);
+
+// Note
+// The slice() method creates a new array.
+// The slice() method does not remove any elements from the source array.
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+var citrus = fruits.slice(1);
+
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+var citrus = fruits.slice(1, 3);
+
+// All JavaScript objects have a toString() method.
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.toString();
---
+++ Iterators/app.js
@@ -0,0 +1,154 @@
+
+// Iterators 
+
+// Iterators are iterable objects (like Arrays).
+
+// Iteratorscan be accessed with simple and efficient code.
+
+// Iterators can be iterated over with for..of loops
+
+
+///// For Of Loop /////
+
+// The JavaScript for..of statement loops through the elements of an iterable object.
+
+// for (variable of iterable) {
+//     // code block to be executed
+//   }
+
+// Iterating is easy to understand.
+// It simply means looping over a sequence of elements.
+// Here are some easy examples:
+
+// Iterating over a String
+// Iterating over an Array
+
+// 1. Iterating Over a String
+
+// You can use a for..of loop to iterate over the elements of a string:
+
+let name = "Maryam";
+
+for (let x of name) {
+  console.log(name);
+  
+}
+
+//2. Iterating Over an Array
+// You can use a for..of loop to iterate over the elements of an Array:
+
+// Example 1
+const letters = ["a","b","c"];
+
+for (const letter of letters) {
+  console.log(letter);
+  
+}
+
+// Example 2
+const numbers = [2,4,6,8];
+
+for (const number of numbers) {
+  console.log(number);
+  
+}
+
+//3. Iterating Over a Set
+// You can use a for..of loop to iterate over the elements of a Set:
+
+const alphabets = new Set(["a","b","c"]);
+
+for (const alphabet of alphabets) {
+  console.log(alphabet);
+  
+}
+
+// Iterating Over a Map
+// You can use a for..of loop to iterate over the elements of a Map:
+
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+
+for (const friut of fruits) {
+  console.log(friut);
+  
+}
+
+
+/////// JavaScript Iterators //////////
+
+// The iterator protocol defines how to produce a sequence of values from an object.
+// An object becomes an iterator when it implements a next() method.
+// The next() method must return an object with two properties:
+
+// value (the next value)
+// done (true or false)
+
+// Note
+// Technically, iterables must implement the Symbol.iterator method.
+// String, Array, TypedArray, Map and Set are all iterables, because their prototype objects have a Symbol.iterator method.
+
+////// Home Made Iterable///////
+// This iterable returns never ending: 10,20,30,40,.... Everytime next() is called:
+
+// Home Made Iterable
+function myNumbers() {
+    let n = 0;
+    return {
+      next: function() {
+        n += 10;
+        return {value:n, done:false};
+      }
+    };
+  }
+
+  // Create Iterable
+  const n = myNumbers();
+  n.next(); // Returns 10
+  n.next(); // Returns 20
+  n.next(); // Returns 30
+
+  
+
+//   The problem with a home made iterable:
+//   It does not support the JavaScript for..of statement.
+//   A JavaScript iterable is an object that has a Symbol.iterator.
+//   The Symbol.iterator is a function that returns a next() function.
+//   An iterable can be iterated over with the code: for (const x of iterable) { }
+
+// Create an Object
+myNumbers = {};
+
+// Make it Iterable
+myNumbers[Symbol.iterator] = function() {
+  let n = 0;
+  done = false;
+  return {
+    next() {
+      n += 10;
+      if (n == 100) {done = true}
+      return {value:n, done:done};
+    }
+  };
+}
+
+// Now you can use for..of
+
+for (const num of myNumbers) {
+  console.log(num);
+  
+}
+
+// The Symbol.iterator method is called automatically by for..of.
+// But we can also do it "manually":
+
+let iterator = myNumbers[Symbol.iterator]();
+
+while (true) {
+  const result = iterator.next();
+  if (result.done) break;
+  // Any Code Here
+}
